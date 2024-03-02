329a13a18a68b5a97fafb333a529146ab82b8947
Samantha Samarua <samantha.samarua@gmail.com>
2024-03-02 21:10:17 +0930

    07 - Added DELETE request in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -52,4 +52,42 @@ module.exports = (app) => {
       });
     });
   });
+
+  // DELETE /api/notes/:id: Delete a note with the specified id.
+  app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile(dbFilePath, (err, data) => {
+      if (err) {
+        // Handle errors when reading the file
+        console.error('Error reading db.json:', err);
+        return res.status(500).json({ error: 'Server Error' });
+      }
+
+      // Parse the JSON data into a JavaScript object
+      let db = JSON.parse(data);
+
+      // Find the index of the note with the specified id in the array
+      const index = db.findIndex(note => note.id === noteId);
+
+      if (index === -1) {
+        // If the note with the specified id is not found, return a 404 error
+        return res.status(404).json({ error: 'Note not found' });
+      }
+      
+      // Uses the Array.splice method to remove note from the db array
+      db.splice(index, 1);
+
+      // Write the updated array of notes back to db.json file
+      fs.writeFile(dbFilePath, JSON.stringify(db), (err) => {
+        if (err) {
+          console.error('Error writing db.json:', err);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        // Response
+        res.json({ message: 'Note deleted' });
+      });
+    });
+  });
 };
